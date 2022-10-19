fe585e91889b14923b328bd10411eb60e2643703
Everton da Rosa <everton3x@gmail.com>
2022-10-19 09:18:57 -0300

    MSC 09/2022

--- 00-configuracoes.py
+++ 00-configuracoes.py
@@ -6,23 +6,23 @@ Configurações
 mapeamento_file = 'mapeamentos.xlsx'
 #mapeamento_file = '.\mun_nc\mapeamentos.xlsx'
 # Caminho para balancete.xlsx do mês da conferência
-msc_atual = r'C:\Users\Everton\OneDrive\Prefeitura\2022\MSC\2022-07\balancete.xls'
+msc_atual = r'C:\Users\Everton\Desktop\Prefeitura\2022\MSC\2022-09\balancete.xls'
 #msc_atual = r'.\mun_nc\balancete.xls'
 #msc_atual = r'teste/msc_atual.xls'
 
 # Caminho para o balancete.xlsx do mês anterior
-msc_anterior = r'C:\Users\Everton\OneDrive\Prefeitura\2022\MSC\2022-06\balancete.xls'
+msc_anterior = r'C:\Users\Everton\Desktop\Prefeitura\2022\MSC\2022-08\balancete.xls'
 #msc_anterior = r'.\mun_nc\anterior.xls'
 #msc_anterior = r'teste/msc_anterior.xls'
 
 
 # Caminho para o BAL_VER.csv geerado pelo pad-converter
-bal_ver = r'C:\Users\Everton\OneDrive\Prefeitura\PAD\2022-07\BAL_VER.csv'
+bal_ver = r'C:\Users\Everton\Desktop\Prefeitura\PAD\2022-09\BAL_VER.csv'
 #bal_ver = r'.\mun_nc\BAL_VER.csv'
 #bal_ver = r'teste/BAL_VER.csv'
 
 # Caminho para o tce_4111.csv geerado pelo pad-converter
-tce_4111 = r'C:\Users\Everton\OneDrive\Prefeitura\PAD\2022-07\tce_4111.csv'
+tce_4111 = r'C:\Users\Everton\Desktop\Prefeitura\PAD\2022-09\tce_4111.csv'
 #tce_4111 = r'.\mun_nc\tce_4111.csv'
 #tce_4111 = r'teste/tce_4111.csv'
 
